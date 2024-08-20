e5df9333616a30b4f080e33cc66e49c45ac0b03b
mihailpoliak <mihailpoliak@mbp-mihail>
2024-08-20 20:40:14 +0300

    Change Post to Get

--- src/main/java/telran/java53/Forum/Dto/DatePeriodDto.java
+++ src/main/java/telran/java53/Forum/Dto/DatePeriodDto.java
@@ -3,8 +3,12 @@ package telran.java53.Forum.Dto;
 
 import java.time.LocalDate;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class DatePeriodDto {
 	LocalDate dateFrom;

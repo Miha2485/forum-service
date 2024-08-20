37d4bae1518ce1324c5fa5538c10b8a2290f92fa
mihailpoliak <mihailpoliak@mbp-mihail>
2024-08-20 21:00:06 +0300

    accounting was started

---
+++ src/main/java/telran/java53/accounting/dto/UserEditDto.java
@@ -0,0 +1,11 @@
+package telran.java53.accounting.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserEditDto {
+	String lastName;
+	String firstName;
+}
---
+++ src/main/java/telran/java53/accounting/dto/UserRegisterDto.java
@@ -0,0 +1,10 @@
+package telran.java53.accounting.dto;
+
+public class UserRegisterDto {
+
+	String login;
+	String password;
+	String firstName;
+	String lastName;
+
+}

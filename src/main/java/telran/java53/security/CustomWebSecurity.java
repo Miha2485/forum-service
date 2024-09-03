9620ed27d16eab22e1430e90da8c9f5278ac6eac
mihailpoliak <mihailpoliak@mbp-mihail>
2024-09-03 18:13:44 +0300

    Spring Security complete

--- src/main/java/telran/java53/security/CustomWebSecurity.java
+++ src/main/java/telran/java53/security/CustomWebSecurity.java
@@ -1,5 +1,19 @@
 package telran.java53.security;
 
-public class CustomWebSecurity {
+import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+import telran.java53.post.dao.PostRepository;
+import telran.java53.post.model.Post;
+
+@Service("customSecurity")
+@RequiredArgsConstructor
+public class CustomWebSecurity {
+	final PostRepository postRepository;
+
+
+	public boolean checkPostAuthor(String postId, String userName) {
+		Post post = postRepository.findById(postId).orElse(null);
+		return post != null && userName.equals(post.getAuthor());
+	}
 }
